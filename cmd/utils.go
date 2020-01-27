c0d5b8cf606bf86d935c905b6f38bf066eb55c7c
Oleg Sadov <oleg.sadov@gmail.com>
2020-01-27 10:55:38 +0300

    Added return codes for command runner.

--- cmd/utils.go
+++ cmd/utils.go
@@ -54,51 +54,53 @@ func run(timeout time.Duration, args []string) {
 	}
 }
 
+func run_out (rerr error, cerr error, sout bytes.Buffer, serr bytes.Buffer) {
+	if rerr != nil {
+		fmt.Println("Command error: ", rerr.Error())
+		if cerr != nil {
+			fmt.Println("Command error: ",
+				cerr.Error())
+		}
+	}
+	fmt.Printf("Stdout:\n%s\n", sout.String())
+	fmt.Printf("Stderr:\n%s\n", serr.String())
+}
+
 // Run shell command with arguments and enviroment variables
-func rune(timeout time.Duration, args []string, env string) {
-	envs := strings.Split(env, " ")
+func rune(timeout time.Duration, args []string, env string) (rerr error, cerr error, stdout bytes.Buffer, stderr bytes.Buffer) {
+	var re error
+	var ce error
+	var sout bytes.Buffer
+	var serr bytes.Buffer
+
 	if len(args) > 0 {
+		envs := strings.Split(env, " ")
 		cmd := args[0]
 		args = args[1:]
 		if timeout != 0 {
-			ctx, cancel := context.WithTimeout(context.Background(),
+			var cancel context.CancelFunc
+			cnt, cancel := context.WithTimeout(context.Background(),
 				timeout)
 			defer cancel()
 
-			cmd := exec.CommandContext(ctx, cmd, args...)
-			var sout bytes.Buffer
-			var serr bytes.Buffer
+			cmd := exec.CommandContext(cnt, cmd, args...)
 			cmd.Stdout = &sout
 			cmd.Stderr = &serr
 			cmd.Env = append(os.Environ(), envs...)
 
-			err := cmd.Run()
-			if err != nil {
-				fmt.Println("Command error: ", err.Error())
-				cerr := ctx.Err()
-				if cerr != nil {
-					fmt.Println("Command error: ",
-						cerr.Error())
-				}
-			}
-			fmt.Printf("Stdout:\n%s\n", sout.String())
-			fmt.Printf("Stderr:\n%s\n", serr.String())
+			re = cmd.Run()
+			ce = cnt.Err()
 		} else {
 			cmd := exec.Command(cmd, args...)
-			var sout bytes.Buffer
-			var serr bytes.Buffer
 			cmd.Stdout = &sout
 			cmd.Stderr = &serr
 			cmd.Env = append(os.Environ(), envs...)
 
-			err := cmd.Run()
-
-			if err != nil {
-				fmt.Println("Command error: ", err.Error())
-			}
-			fmt.Printf("Stdout:\n%s\n", sout.String())
-			fmt.Printf("Stderr:\n%s\n", serr.String())
+			re = cmd.Run()
+		}
+		if verbose {
+			run_out(re, ce, sout, serr)
 		}
 	}
+	return re, ce, sout, serr
 }
-
