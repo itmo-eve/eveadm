6fb9b8ac0c1a55ea61a4d8367c0a94b480f88360
giggsoff <giggsoff@gmail.com>
2020-01-31 17:02:22 +0300

    more information in bash tests

--- eveadm.xen.test.sh
+++ eveadm.xen.test.sh
@@ -3,6 +3,11 @@ if ! [ $(id -u) = 0 ]; then
    echo "The script need to be run as root." >&2
    exit 1
 fi
+echo 'home_dir=/tmp/xen_test
+rm -rf "$home_dir"
+mkdir "$home_dir"
+echo "$home_dir"
+name="testxen"'
 home_dir=/tmp/xen_test
 rm -rf "$home_dir"
 mkdir "$home_dir"
@@ -11,7 +16,17 @@ name="testxen"
 echo ========================================
 echo "download image and create config"
 echo ========================================
+echo 'wget http://download.cirros-cloud.net/0.4.0/cirros-0.4.0-x86_64-disk.img -O "$home_dir"/cirros.qcow2'
 wget http://download.cirros-cloud.net/0.4.0/cirros-0.4.0-x86_64-disk.img -O "$home_dir"/cirros.qcow2
+echo 'cat << EOF > "$home_dir"/config.cfg
+name = "$name"
+bootloader = "pygrub"
+extra = "console=hvc0 root=/dev/xvda1"
+memory = 128
+vcpus = 1
+vif = [ '\''bridge=xenbr0'\'' ]
+disk = [ '\''$home_dir/cirros.qcow2,qcow2,xvda,rw'\'' ]
+EOF'
 cat << EOF > "$home_dir"/config.cfg
 name = "$name"
 bootloader = "pygrub"
@@ -21,10 +36,12 @@ vcpus = 1
 vif = [ 'bridge=xenbr0' ]
 disk = [ '$home_dir/cirros.qcow2,qcow2,xvda,rw' ]
 EOF
+echo 'brctl show|grep xenbr0||brctl addbr xenbr0'
 brctl show|grep xenbr0||brctl addbr xenbr0
 echo ========================================
 echo "create vm"
 echo ========================================
+echo './eveadm xen create --xen-cfg-filename="$home_dir"/config.cfg --paused'
 ./eveadm xen create --xen-cfg-filename="$home_dir"/config.cfg --paused
 echo ========================================
 echo "sleep 5"
@@ -33,15 +50,19 @@ sleep 5
 echo ========================================
 echo "domid vm"
 echo ========================================
+echo 'domuuid=$(./eveadm xen info --domname $name)'
 domuuid=$(./eveadm xen info --domname $name)
+echo 'echo "$domuuid"'
 echo "$domuuid"
 echo ========================================
 echo "start vm"
 echo ========================================
+echo './eveadm xen start "$domuuid"'
 ./eveadm xen start "$domuuid"
 echo ========================================
 echo "vm info"
 echo ========================================
+echo './eveadm xen info "$domuuid"'
 ./eveadm xen info "$domuuid"
 echo ========================================
 echo "sleep 5"
@@ -50,9 +71,11 @@ sleep 5
 echo ========================================
 echo "vm stop"
 echo ========================================
+echo './eveadm xen stop "$domuuid"'
 ./eveadm xen stop "$domuuid"
 echo ========================================
 echo "vm delete"
 echo ========================================
+echo './eveadm xen delete "$domuuid"'
 ./eveadm xen delete "$domuuid"
 echo ========================================
