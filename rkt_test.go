d9149d203648c0fa6f2f75c12dd4d46420f8c85f
giggsoff <giggsoff@gmail.com>
2020-02-04 15:51:01 +0300

    refactoring

--- rkt_test.go
+++ rkt_test.go
@@ -12,11 +12,27 @@ import (
 	"time"
 )
 
-var rktTestDir = "pods_test"
-var binaryName = "eveadm"
-var imageHash = "sha512-5bee98b12bc8eb63425d8af93a8dc1be"
-
 func TestRktSequence(t *testing.T) {
+
+	type rktTestContext struct {
+		containerHash string
+		imageHash     string
+	}
+	type rktTestRun struct {
+		name          string
+		args          func() []string
+		contains      func() []string
+		omit          func() []string
+		useEveAdm     bool
+		sleep         time.Duration
+		resultProcess func(result string)
+	}
+
+	var rktCtx rktTestContext
+
+	var rktTestDir = "pods_test"
+	var binaryName = "eveadm"
+
 	hasXL := false
 	dir, err := os.Getwd()
 	xlcmd := exec.Command("which", "xl")
@@ -46,44 +62,192 @@ func TestRktSequence(t *testing.T) {
 		defer os.RemoveAll(dname)
 		t.Logf("Rkt folder: %s", dname)
 
-		containerHash := ""
-
-		tests := []struct {
-			name      string
-			args      []string
-			contains  []string
-			omit      []string
-			useEveAdm bool
-			sleep     time.Duration
-		}{
-			{"image_create", []string{"rkt", "create", "--image=true", "docker://library/alpine:3.11", "--dir=" + dname}, []string{"sha512", imageHash}, []string{}, true, 0},
-			{"image_list", []string{"rkt", "list", "--image=true", "--dir=" + dname, "--no-legend=true"}, []string{"sha512", "registry-1.docker.io/library/alpine:3.11"}, []string{"LAST USED"}, true, 0},
-			{"image_info", []string{"rkt", "info", "--image=true", imageHash, "--dir=" + dname}, []string{}, []string{}, true, 0},
-			{"container_create", []string{"systemd-run", path.Join(dir, binaryName), "rkt", "create", imageHash, "--dir=" + dname, "--no-overlay=true", "--stage1-path="}, []string{"Running as unit"}, []string{}, false, 0},
-			{"container_list", []string{"rkt", "list", "--dir=" + dname, "--no-legend=true"}, []string{"alpine"}, []string{"ID"}, true, 10},
-			{"container_info", []string{"rkt", "info", "--dir=" + dname, containerHash}, []string{"state"}, []string{}, true, 0},
-			{"container_start", []string{"rkt", "start", "--dir=" + dname, "--stage1-type=common", containerHash}, []string{"Not implemented"}, []string{}, true, 0},
-			{"container_stop", []string{"rkt", "stop", "--dir=" + dname, containerHash}, []string{containerHash}, []string{}, true, 0},
-			{"container_delete", []string{"rkt", "delete", "--dir=" + dname, containerHash}, []string{containerHash}, []string{}, true, 0},
-			{"image_delete", []string{"rkt", "delete", "--image=true", "--dir=" + dname, imageHash}, []string{imageHash}, []string{}, true, 0},
-		}
+		var tests []rktTestRun
+		imageCreateTest := rktTestRun{
+			"image_create",
+			func() []string {
+				return []string{"rkt", "create", "--image=true", "docker://library/alpine:3.11", "--dir=" + dname}
+			},
+			func() []string {
+				return []string{"sha512", rktCtx.imageHash}
+			}, func() []string {
+				return []string{}
+			},
+			true,
+			0,
+			func(result string) { rktCtx.imageHash = result }}
+		tests = append(tests, imageCreateTest)
+		imageInfo := rktTestRun{
+			"image_info",
+			func() []string {
+				return []string{"rkt", "info", "--image=true", rktCtx.imageHash, "--dir=" + dname}
+			},
+			func() []string {
+				return []string{}
+			},
+			func() []string {
+				return []string{}
+			},
+			true,
+			0,
+			func(result string) {}}
+		tests = append(tests, imageInfo)
+		containerCreate := rktTestRun{
+			"container_create",
+			func() []string {
+				return []string{"systemd-run", path.Join(dir, binaryName), "rkt", "create", rktCtx.imageHash, "--dir=" + dname, "--no-overlay=true", "--stage1-path="}
+			},
+			func() []string {
+				return []string{"Running as unit"}
+			},
+			func() []string {
+				return []string{}
+			},
+			false,
+			0,
+			func(result string) {}}
 		if hasXL {
-			//fix container_create for stage1-xen.aci
-			tests[3].args = []string{"rkt", "create", imageHash, "--dir=" + dname, "--paused=true"}
-			tests[3].useEveAdm = true
-			tests[3].contains = []string{}
-			tests[3].omit = []string{"Running as unit"}
+			containerCreate = rktTestRun{
+				"container_create",
+				func() []string {
+					return []string{"rkt", "create", rktCtx.imageHash, "--dir=" + dname, "--paused=true"}
+				},
+				func() []string {
+					return []string{}
+				}, func() []string {
+					return []string{"Running as unit"}
+				},
+				true,
+				0,
+				func(result string) {}}
 		}
+		tests = append(tests, containerCreate)
+		containerList := rktTestRun{
+			"container_list",
+			func() []string {
+				return []string{"rkt", "list", "--dir=" + dname, "--no-legend=true"}
+			},
+			func() []string {
+				return []string{"alpine"}
+			},
+			func() []string {
+				return []string{"ID"}
+			},
+			true,
+			10,
+			func(result string) {
+				rktCtx.containerHash = strings.Split(result, "\t")[0]
+			}}
+		tests = append(tests, containerList)
+		containerInfo := rktTestRun{
+			"container_info",
+			func() []string {
+				return []string{"rkt", "info", "--dir=" + dname, rktCtx.containerHash}
+			},
+			func() []string {
+				return []string{"state", "alpine"}
+			},
+			func() []string {
+				return []string{}
+			},
+			true,
+			0,
+			func(result string) {
+				re := regexp.MustCompile(`"name":[ ]?"([a-z0-9-]*)"`)
+				resultsFind := re.FindSubmatch([]byte(result))
+				if resultsFind != nil && len(resultsFind) == 2 {
+					rktCtx.containerHash = string(resultsFind[1])
+				}
+			}}
+		tests = append(tests, containerInfo)
+		containerStart := rktTestRun{
+			"container_start",
+			func() []string {
+				return []string{"rkt", "start", "--dir=" + dname, "--stage1-type=common", rktCtx.containerHash}
+			},
+			func() []string {
+				return []string{"Not implemented"}
+			},
+			func() []string {
+				return []string{}
+			},
+			true,
+			0,
+			func(result string) {}}
+		if hasXL {
+			containerStart = rktTestRun{
+				"container_start",
+				func() []string {
+					return []string{"rkt", "start", "--dir=" + dname, "--stage1-type=xen", rktCtx.containerHash}
+				},
+				func() []string {
+					return []string{}
+				},
+				func() []string {
+					return []string{"Not implemented"}
+				},
+				true,
+				0,
+				func(result string) {}}
+		}
+		tests = append(tests, containerStart)
+		containerStop := rktTestRun{
+			"container_stop",
+			func() []string {
+				return []string{"rkt", "stop", "--dir=" + dname, rktCtx.containerHash}
+			},
+			func() []string {
+				return []string{rktCtx.containerHash}
+			},
+			func() []string {
+				return []string{}
+			},
+			true,
+			0,
+			func(result string) {}}
+		tests = append(tests, containerStop)
+		containerDelete := rktTestRun{
+			"container_delete",
+			func() []string {
+				return []string{"rkt", "delete", "--dir=" + dname, rktCtx.containerHash}
+			},
+			func() []string {
+				return []string{rktCtx.containerHash}
+			},
+			func() []string {
+				return []string{}
+			},
+			true,
+			0,
+			func(result string) {}}
+		tests = append(tests, containerDelete)
+		imageDelete := rktTestRun{
+			"image_delete",
+			func() []string {
+				return []string{"rkt", "delete", "--image=true", "--dir=" + dname, rktCtx.imageHash}
+			},
+			func() []string {
+				return []string{rktCtx.imageHash}
+			},
+			func() []string {
+				return []string{}
+			},
+			true,
+			0,
+			func(result string) {}}
+		tests = append(tests, imageDelete)
+
 		for _, tt := range tests {
 			t.Run(tt.name, func(t *testing.T) {
 				if tt.sleep > 0 {
 					time.Sleep(tt.sleep * time.Second)
 				}
 
-				cmd := exec.Command(path.Join(dir, binaryName), tt.args...)
+				args := tt.args()
+				cmd := exec.Command(path.Join(dir, binaryName), args...)
 				if !tt.useEveAdm {
-					cmdFile := tt.args[0]
-					cmdArgs := tt.args[1:]
+					cmdFile := args[0]
+					cmdArgs := args[1:]
 					cmd = exec.Command(cmdFile, cmdArgs...)
 				}
 				t.Log(strings.Join(cmd.Args, " "))
@@ -94,39 +258,18 @@ func TestRktSequence(t *testing.T) {
 
 				actual := strings.TrimSpace(string(output))
 
-				for _, expected := range tt.contains {
+				for _, expected := range tt.contains() {
 					if !strings.Contains(actual, expected) {
 						t.Fatalf("actual = %s, expected = %s", actual, expected)
 					}
 				}
-				for _, omitted := range tt.omit {
+				for _, omitted := range tt.omit() {
 					if strings.Contains(actual, omitted) {
 						t.Fatalf("actual = %s, omitted = %s", actual, omitted)
 					}
 				}
-				if tt.name == "container_list" {
-					containerHash = strings.Split(actual, "\t")[0]
-					//fix container_start for actual containerHash
-					tests[5].args = []string{"rkt", "info", "--dir=" + dname, containerHash}
-				}
-				if tt.name == "container_info" {
-					re := regexp.MustCompile(`"name":[ ]?"([a-z0-9-]*)"`)
-					resultsFind := re.FindSubmatch([]byte(actual))
-					if resultsFind != nil && len(resultsFind) == 2 {
-						containerHash = string(resultsFind[1])
-						if hasXL {
-							tests[6].args = []string{"rkt", "start", "--dir=" + dname, containerHash}
-							tests[6].contains = []string{}
-							tests[6].omit = []string{"Not implemented"}
-						} else {
-							tests[6].args = []string{"rkt", "start", "--dir=" + dname, "--stage1-type=common", containerHash}
-						}
-						tests[7].args = []string{"rkt", "stop", "--dir=" + dname, containerHash}
-						tests[7].contains = []string{containerHash}
-						tests[8].args = []string{"rkt", "delete", "--dir=" + dname, containerHash}
-						tests[8].contains = []string{containerHash}
-					}
-				}
+				t.Log(actual)
+				tt.resultProcess(actual)
 			})
 		}
 
